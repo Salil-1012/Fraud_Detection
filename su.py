b31c45401dc2de6ce7ad0d36a0fe16f268fc5540
Salil-1012 <salilchandan00@gmail.com>
2023-07-17 10:23:05 +0530

    new function is added

--- su.py
+++ su.py
@@ -1,4 +1,11 @@
 a = eval(input('Enter the number'))
 print(sum(a))
 
-# lsdnlgksnlkasdgmlgnadlgkndlng
+def sum(a,b):
+    print(a+b)
+def multiple(a,b):
+    print(a*b)
+def divide(a,b):
+    print(a//b)
+def remainder(a,b):
+    print(a%b)
